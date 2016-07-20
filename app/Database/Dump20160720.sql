9a5d5278f6a0193cb156c123088e0d477cbca997
Antony Kalogeropoulos <anthonykalogeropoulos@gmail.com>
2016-07-20 18:29:23 +0300

    Update DB dump

--- app/Database/Dump20160720.sql
+++ app/Database/Dump20160720.sql
@@ -1,4 +1,3 @@
-# noinspection SqlNoDataSourceInspectionForFile
 -- MySQL dump 10.13  Distrib 5.7.12, for Linux (x86_64)
 --
 -- Host: 127.0.0.1    Database: fab
@@ -15,6 +14,32 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `carousel`
+--
+
+DROP TABLE IF EXISTS `carousel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `carousel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `included` int(11) NOT NULL DEFAULT '0',
+  `position` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `carousel`
+--
+
+LOCK TABLES `carousel` WRITE;
+/*!40000 ALTER TABLE `carousel` DISABLE KEYS */;
+INSERT INTO `carousel` VALUES (2,'masonry_col2_1.jpg',1,2),(22,'Capture.PNG',1,3);
+/*!40000 ALTER TABLE `carousel` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `items`
@@ -82,4 +107,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-07-19 18:20:12
+-- Dump completed on 2016-07-20 18:27:19
